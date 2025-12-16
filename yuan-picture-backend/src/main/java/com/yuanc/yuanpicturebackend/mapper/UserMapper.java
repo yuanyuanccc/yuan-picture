65cc49f10aa1601af68ade815032bf1d89691240
Yuanc <718297950@qq,com>
2025-12-16 16:01:30 +0800

    2.用户模块后端

---
+++ yuan-picture-backend/src/main/java/com/yuanc/yuanpicturebackend/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.yuanc.yuanpicturebackend.mapper;
+
+import com.yuanc.yuanpicturebackend.model.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author Yuanc
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2025-12-16 15:26:26
+* @Entity com.yuanc.yuanpicturebackend.model.entity.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
---
+++ yuan-picture-backend/src/main/java/com/yuanc/yuanpicturebackend/service/UserService.java
@@ -0,0 +1,74 @@
+package com.yuanc.yuanpicturebackend.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.yuanc.yuanpicturebackend.model.dto.user.UserQueryRequest;
+import com.yuanc.yuanpicturebackend.model.entity.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.yuanc.yuanpicturebackend.model.vo.LoginUserVO;
+import com.yuanc.yuanpicturebackend.model.vo.UserVO;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+/**
+* @author Yuanc
+* @description 针对表【user(用户)】的数据库操作Service
+* @createDate 2025-12-16 15:26:26
+*/
+public interface UserService extends IService<User> {
+
+    /**
+     * 用户注册
+     *
+     * @param userAccount   用户账户
+     * @param userPassword  用户密码
+     * @param checkPassword 校验密码
+     * @return 新用户 id
+     */
+    long userRegister(String userAccount, String userPassword, String checkPassword);
+
+
+    /**
+     * 用户登录
+     *
+     * @param userAccount  用户账户
+     * @param userPassword 用户密码
+     * @param request
+     * @return 脱敏后的用户信息
+     */
+    LoginUserVO userLogin(String userAccount, String userPassword, HttpServletRequest request);
+
+
+    /**
+     * 获取当前登录用户
+     *
+     * @param request
+     * @return
+     */
+    User getLoginUser(HttpServletRequest request);
+
+
+    /**
+     * 获取脱敏的已登录用户信息
+     *
+     * @return
+     */
+    LoginUserVO getLoginUserVO(User user);
+
+    /**
+     * 用户注销
+     *
+     * @param request
+     * @return
+     */
+    boolean userLogout(HttpServletRequest request);
+
+
+    UserVO getUserVO(User user);
+
+    List<UserVO> getUserVOList(List<User> userList);
+
+    QueryWrapper<User> getQueryWrapper(UserQueryRequest userQueryRequest);
+
+    String getEncryptPassword(String userPassword);
+}
